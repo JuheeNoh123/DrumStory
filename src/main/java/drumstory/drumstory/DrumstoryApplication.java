fbaaeb922b7fb4435035ea23b18fc4010b676c5c
juheeNoh123 <juhee10131013@gmail.com>
2025-02-21 01:53:36 +0900

    first commit

---
+++ src/main/java/drumstory/drumstory/DrumstoryApplication.java
@@ -0,0 +1,13 @@
+package drumstory.drumstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrumstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrumstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/drumstory/drumstory/DrumstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package drumstory.drumstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrumstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
