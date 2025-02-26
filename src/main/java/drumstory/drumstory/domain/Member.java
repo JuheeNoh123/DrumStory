95b9da3e75f2a48e31c57d7f2b939cece6fc61b8
juheeNoh123 <juhee10131013@gmail.com>
2025-02-26 22:08:35 +0900

    member domain

---
+++ src/main/java/drumstory/drumstory/domain/Member.java
@@ -0,0 +1,15 @@
+package drumstory.drumstory.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+}
---
+++ src/main/java/drumstory/drumstory/domain/RoleType.java
@@ -0,0 +1,5 @@
+package drumstory.drumstory.domain;
+
+public enum RoleType {
+    ROLE_ADMIN, ROLE_MEMBER
+}
