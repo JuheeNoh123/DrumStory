386985a71d6c2d9a190efbe9508dcb8aea6978a7
juheeNoh123 <juhee10131013@gmail.com>
2025-03-03 21:10:03 +0900

    SEPERATE To ReservationService

--- src/main/java/drumstory/drumstory/DTO/ReservationDTO.java
+++ src/main/java/drumstory/drumstory/DTO/ReservationDTO.java
@@ -39,4 +39,34 @@ public class ReservationDTO {
         private String resDay;
 
     }
+
+    @Data
+    public static class ReservateTimeRoomReq {
+        @Schema(description = "예약 시간 리스트", example = "[ \"오전 HH:MM\",\"오후 HH:MM\"]")
+        private List<String> times;
+        @Schema(description = "예약날짜", example = "YYYY-MM-DD")
+        private String resDate;
+        @Schema(description = "예약한 방", example = "1")
+        private int roomNum;
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    public static class ReservateTimeRoomRes {
+        @Schema(description = "예약자명", example = "노주희")
+        private String name;
+//        @Schema(description = "선택한 예약 시간 리스트", example = "오전 HH:MM")
+//        private List<String> selectTimes;
+        @Schema(description = "예약 시작 시간", example = "오전 HH:MM")
+        private String startTime;
+        @Schema(description = "예약 끝 시간", example = "오전 HH:MM")
+        private String endTime;
+        @Schema(description = "예약날짜", example = "YYYY-MM-DD")
+        private LocalDate resDate;
+        @Schema(description = "예약요일", example = "Monday")
+        private String resDay;
+        @Schema(description = "예약한 방", example = "1")
+        private int roomNum;
+    }
 }
---
+++ src/main/java/drumstory/drumstory/controller/MemberReservationController.java
@@ -0,0 +1,54 @@
+package drumstory.drumstory.controller;
+
+import drumstory.drumstory.DTO.ReservationDTO;
+import drumstory.drumstory.domain.Member;
+import drumstory.drumstory.service.MemberService;
+import drumstory.drumstory.service.ReservationService;
+import drumstory.drumstory.service.TimeTable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@Tag(name = "회원 페이지-예약 관련(조회,예약,취소)")
+public class MemberReservationController {
+    private final MemberService memberService;
+    private final ReservationService reservationService;
+    private final TimeTable availableTime;
+
+    @GetMapping("/schedule")
+    public List<ReservationDTO.ScheduleRes> getSchedule(HttpServletRequest header) {
+
+        availableTime.getAvailableTimes();
+        return null;
+    }
+
+    @Operation(summary = "시간 예약 (주희)", description = "해더에 토큰 필요",
+            responses = {@ApiResponse(responseCode = "200", description = "시간 선택 완료"),
+                    @ApiResponse(responseCode = "400", description = "1. 최대 두 개의 시간만 선택 가능 <br>2. 예약 시간을 선택해주세요(빈 리스트 일 경우)<br>3. 연속된 시간으로만 선택 가능합니다"),
+            })
+    @PostMapping("/reservate/time")
+    public ResponseEntity<ReservationDTO.ReservateTimeRes> ReservateTime(HttpServletRequest header, @RequestBody ReservationDTO.ReservateTimeReq request ) {
+        Member member = memberService.tokenToMember(header);
+        ReservationDTO.ReservateTimeRes reservateTimeres = reservationService.reservateTime(member, request.getTimes(), request.getResDate());
+        return ResponseEntity.status(HttpStatus.OK).body(reservateTimeres);
+    }
+
+    @PostMapping("/reservate/room")
+    public  ResponseEntity<ReservationDTO.ReservateTimeRoomRes> saveReservation(HttpServletRequest header, @RequestBody ReservationDTO.ReservateTimeRoomReq request) {
+        Member member = memberService.tokenToMember(header);
+        return null;
+
+    }
+}
